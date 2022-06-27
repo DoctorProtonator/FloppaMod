439529c6f81b00f9c35ccc57ab6e19ba17f0fc0e
Pierluigi <Pierluigi@Protonic-Engine-II>
2022-06-27 21:41:52 +0200

    modifications in SeraplopStatue and SeraphlopStatueEntity

--- FloppaMod/src/main/java/com/doctorprotonator/floppamod/block/SeraphlopStatue.java
+++ FloppaMod/src/main/java/com/doctorprotonator/floppamod/block/SeraphlopStatue.java
@@ -1,12 +1,12 @@
 package com.doctorprotonator.floppamod.block;
 
+import com.doctorprotonator.floppamod.block.block_entities.SeraphlopStatueEntity;
 import com.doctorprotonator.floppamod.init.BlockEntityInit;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.context.BlockPlaceContext;
@@ -16,15 +16,14 @@ import net.minecraft.world.level.block.DirectionalBlock;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.phys.BlockHitResult;
 
 public class SeraphlopStatue extends DirectionalBlock implements EntityBlock
 {
-	public static final BooleanProperty ACTIVATED = BooleanProperty.create("clicked");
-	
 	public SeraphlopStatue(Properties properties)
 	{
 		super(properties);
@@ -35,6 +34,14 @@ public class SeraphlopStatue extends DirectionalBlock implements EntityBlock
 	{
 		return BlockEntityInit.SERAPHLOP_STATUE_ENTITY.get().create(blockPos, blockState);
 	}
+	
+	/*@Override
+	public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state,
+			BlockEntityType<T> type)
+	{
+		return level.isClientSide ? null : (level0, pos, state0, blockEntity) ->
+		((SeraphlopStatueEntity)blockEntity).tick();
+	}*/
 	
 	@Override
 	public RenderShape getRenderShape(BlockState blockState)
@@ -50,7 +57,6 @@ public class SeraphlopStatue extends DirectionalBlock implements EntityBlock
 	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder)
 	{
 		builder.add(FACING);
-		builder.add(ACTIVATED);
 	}
 	
 	@Override
@@ -62,23 +68,10 @@ public class SeraphlopStatue extends DirectionalBlock implements EntityBlock
 			ItemStack itemstack = player.getItemInHand(hand);
 		    if(itemstack.is(Items.BLAZE_ROD))
 		    {
-		    	boolean currentState = blockState.getValue(ACTIVATED);
-				level.setBlock(blockPos, blockState.setValue(ACTIVATED, !currentState), 3);
+		    	level.getBlockEntity(blockPos, BlockEntityInit.SERAPHLOP_STATUE_ENTITY.get()).get().isActivated = true;
+		    	level.getBlockEntity(blockPos, BlockEntityInit.SERAPHLOP_STATUE_ENTITY.get()).get().setChanged();
 		    }
 		}
 		return InteractionResult.SUCCESS;
 	}
-	
-	/*
-	private void spawnBossAndBreakBlock()
-	{
-		SeraphlopEntity boss = new SeraphlopEntity(EntityInit.SERAPHLOP.get(), this.level);
-		double x = blockPos.getX();
-		double y = blockPos.getY();
-		double z = blockPos.getZ();
-		
-		this.level.addFreshEntity(boss);
-		boss.setAggressive(true);
-		boss.setPosRaw(x, y + 3, z);
-	}*/
 }
--- FloppaMod/src/main/java/com/doctorprotonator/floppamod/block/block_entities/SeraphlopStatueEntity.java
+++ FloppaMod/src/main/java/com/doctorprotonator/floppamod/block/block_entities/SeraphlopStatueEntity.java
@@ -1,6 +1,5 @@
 package com.doctorprotonator.floppamod.block.block_entities;
 
-import com.doctorprotonator.floppamod.block.SeraphlopStatue;
 import com.doctorprotonator.floppamod.entity.SeraphlopEntity;
 import com.doctorprotonator.floppamod.init.BlockEntityInit;
 import com.doctorprotonator.floppamod.init.EntityInit;
@@ -8,10 +7,10 @@ import com.doctorprotonator.floppamod.init.EntityInit;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -26,6 +25,9 @@ public class SeraphlopStatueEntity extends BlockEntity implements IAnimatable
 	BlockState blockState;
 	double eventKeyframe = 360d;
 	
+	public boolean isActivated = false;
+	private boolean bossSpawned = false;
+	
 	private AnimationFactory factory = new AnimationFactory(this);
 	
 	public SeraphlopStatueEntity(BlockPos blockPos, BlockState blockState)
@@ -33,6 +35,20 @@ public class SeraphlopStatueEntity extends BlockEntity implements IAnimatable
 		super(BlockEntityInit.SERAPHLOP_STATUE_ENTITY.get(), blockPos, blockState);
 		this.blockPos = blockPos;
 		this.blockState = blockState;
+	}
+	
+	@Override
+	protected void saveAdditional(CompoundTag nbt)
+	{
+		super.saveAdditional(nbt);
+		nbt.putBoolean("bossSpawned", bossSpawned);
+	}
+	
+	@Override
+	public void load(CompoundTag nbt)
+	{
+		super.load(nbt);
+		nbt.getBoolean("bossSpawned");
 	}
 
 	@Override
@@ -44,7 +60,7 @@ public class SeraphlopStatueEntity extends BlockEntity implements IAnimatable
 	
 	private <E extends SeraphlopStatueEntity & IAnimatable> PlayState predicate(AnimationEvent<E> event)
     {
-        if(event.getAnimatable().getBlockState().getValue(SeraphlopStatue.ACTIVATED) == false)
+        if(!isActivated)
         {
             event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.seraphlop_statue.idle", false));
         }
@@ -59,8 +75,6 @@ public class SeraphlopStatueEntity extends BlockEntity implements IAnimatable
                 {
                     player.displayClientMessage(new TextComponent("DO NOT BE AFRAID."), true);
                 }
-                spawnBoss();
-                System.out.println("Boss has been spawned");
             }
         }
         return PlayState.CONTINUE;
@@ -82,5 +96,10 @@ public class SeraphlopStatueEntity extends BlockEntity implements IAnimatable
 	public AnimationFactory getFactory()
 	{
 		return this.factory;
+	}
+
+	public static void tick()
+	{
+		
 	}
 }
